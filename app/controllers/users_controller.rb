e13de7c9142d26407f00e054804c9569d4be09c2
sae-yajima <y.sae1338@icloud.com>
2021-05-24 19:03:08 +0900

    protospace指摘箇所の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         @name = @user.name
         @prototypes = @user.prototypes
+
+        
     end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :prototype  # tweetsテーブルとのアソシエーション
+  belongs_to :prototype  
   belongs_to :user
 
   validates :text, presence: true
